86a1e2043e47c723dfc9629dc3e1113ebfcb303b
mrhetao7 <2459713449@qq.com>
2018-10-19 11:27:47 +0800

    Spring Bean的依赖注入，注解实现和Servlet的整合

---
+++ spring4x1/src/main/java/cap/action/AdminServlet.java
@@ -0,0 +1,33 @@
+package cap.action;
+
+import cap.service.AdminService;
+import org.springframework.context.ApplicationContext;
+import org.springframework.web.context.support.WebApplicationContextUtils;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+@WebServlet("/admin")
+public class AdminServlet extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        this.doPost(req,resp);
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        req.setCharacterEncoding("utf-8");
+        resp.setContentType("text/html; charset=utf-8");
+        WebApplicationContextUtils.getWebApplicationContext(getServletContext());
+        ApplicationContext context = WebApplicationContextUtils.getWebApplicationContext(getServletContext());
+        AdminService adminService = (AdminService) context.getBean("adminService");
+        PrintWriter out = resp.getWriter();
+        out.print(adminService.SayHello());
+        out.close();
+    }
+}
---
+++ spring4x1/src/main/java/cap/service/impl/AdminServiceImpl.java
@@ -0,0 +1,17 @@
+package cap.service.impl;
+
+import cap.dao.AdminDAO;
+import cap.service.AdminService;
+import org.springframework.stereotype.Service;
+
+import javax.annotation.Resource;
+
+@Service(value = "adminService")
+public class AdminServiceImpl implements AdminService {
+    @Resource
+    private AdminDAO adminDAO;
+    @Override
+    public String SayHello() {
+        return adminDAO.SayHello();
+    }
+}
