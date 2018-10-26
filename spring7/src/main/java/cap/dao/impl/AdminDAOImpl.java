31d34e131e40ce5d3cc1c14bb5755e21af3136f8
mrhetao7 <2459713449@qq.com>
2018-10-26 11:36:54 +0800

    Spring JDBC的使用

---
+++ spring7/src/main/java/cap/dao/impl/AdminDAOImpl.java
@@ -0,0 +1,34 @@
+package cap.dao.impl;
+
+import cap.bean.Admin;
+import cap.dao.AdminDAO;
+import cap.util.DBCon;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.RowMapper;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.List;
+
+public class AdminDAOImpl implements AdminDAO {
+    private JdbcTemplate jdbcTemplate=new JdbcTemplate(DBCon.getDataSource());
+
+    public class MyRowMappe implements RowMapper<Admin>{
+
+        @Override
+        public Admin mapRow(ResultSet resultSet, int i) throws SQLException {
+            Admin admin=new Admin();
+            admin.setId(resultSet.getInt("id"));
+            admin.setUsername(resultSet.getString("username"));
+            admin.setPassword(resultSet.getString("password"));
+            return admin;
+        }
+    }
+
+    @Override
+    public List<Admin> findAdmins() {
+        String sql="select * from gl order by id";
+        List<Admin> adminList=jdbcTemplate.query(sql,new MyRowMappe());
+        return adminList;
+    }
+}
---
+++ spring7/src/main/java/cap/util/DBCon.java
@@ -0,0 +1,18 @@
+package cap.util;
+
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
+
+import java.sql.DriverManager;
+
+public class DBCon {
+    private static DriverManagerDataSource dataSource;
+
+    public static  DriverManagerDataSource getDataSource(){
+        dataSource=new DriverManagerDataSource();
+        dataSource.setDriverClassName("com.mysql.jdbc.Driver");
+        dataSource.setUrl("jdbc:mysql://localhost:3306/java");
+        dataSource.setUsername("root");
+        dataSource.setPassword("173170");
+        return  dataSource;
+    }
+}
