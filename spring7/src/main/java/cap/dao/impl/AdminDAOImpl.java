24226f9cb15af9c73620b4f767ee497099df4b9d
mrhetao7 <2459713449@qq.com>
2018-11-16 11:53:56 +0800

    Spring JDBC,JdbcTemplate的使用

--- spring7/src/main/java/cap/dao/impl/AdminDAOImpl.java
+++ spring7/src/main/java/cap/dao/impl/AdminDAOImpl.java
@@ -13,12 +13,12 @@ import java.util.List;
 public class AdminDAOImpl implements AdminDAO {
     private JdbcTemplate jdbcTemplate=new JdbcTemplate(DBCon.getDataSource());
 
-    public class MyRowMappe implements RowMapper<Admin>{
+    public class MyRowMappe implements RowMapper<Admin>{             //类MyRowMapper 封装获得参数
 
         @Override
         public Admin mapRow(ResultSet resultSet, int i) throws SQLException {
             Admin admin=new Admin();
-            admin.setId(resultSet.getInt("id"));
+            admin.setId(resultSet.getInt("id"));   //得到参数
             admin.setUsername(resultSet.getString("username"));
             admin.setPassword(resultSet.getString("password"));
             return admin;
--- spring7/src/main/java/cap/util/DBCon.java
+++ spring7/src/main/java/cap/util/DBCon.java
@@ -8,7 +8,7 @@ public class DBCon {
     private static DriverManagerDataSource dataSource;
 
     public static  DriverManagerDataSource getDataSource(){
-        dataSource=new DriverManagerDataSource();
+        dataSource=new DriverManagerDataSource();   //必须要初始化
         dataSource.setDriverClassName("com.mysql.jdbc.Driver");
         dataSource.setUrl("jdbc:mysql://localhost:3306/java");
         dataSource.setUsername("root");
